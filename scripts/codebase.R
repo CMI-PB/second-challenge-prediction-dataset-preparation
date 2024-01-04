41330d200d4b00a8ddbae85817e071b9d5990e34
Pramod Shinde <pshinde@lji.org>
2024-01-04 15:12:50 -0800

    test commit

---
+++ scripts/codebase.R
@@ -0,0 +1,12 @@
+###############################################################################
+#' Load packages and required directory paths
+###############################################################################
+
+#install.packages("pacman")
+library(pacman)
+
+p_load(devtools, tidyverse, Hmisc, BiocManager)
+p_load(corrplot, ggpubr)
+
+
+## Read all housekeeping functions
